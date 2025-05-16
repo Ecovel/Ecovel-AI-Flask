ad2ed399254199773e61c3a6741760f58120aba6
RohEunSeo <esroh1124@naver.com>
2025-05-16 23:18:53 +0900

    no message

--- app/utils/image_utils.py
+++ app/utils/image_utils.py
@@ -1,11 +1,8 @@
-# app/utils/image_utils.py
 import numpy as np
 from PIL import Image
 
 def preprocess_image(image: Image.Image, target_size=(224, 224)):
-    """
-    PIL Image → numpy array, 0~1 정규화, 배치 차원 추가
-    """
+ 
     image = image.resize(target_size)
     arr   = np.array(image) / 255.0
     return np.expand_dims(arr, axis=0)
